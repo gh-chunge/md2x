92bbb7a4433fa10910bbc40bf9da7b85afa88737
春哥 <hi@chun.ge>
2020-07-02 19:21:45 +0800

    优化

--- example/main.js
+++ example/main.js
@@ -2,25 +2,15 @@
 document.createElement('annotation');
 
 ty.set({
-    // 模块所在路径
-    //（注意，如果是script单独引入layui.js，无需设定该参数。），一般情况下可以无视
-    base:       'http://localhost:99/Tyui@0.1/js/libs/', // 扩展模块所在目录
-    debug:      true,       // 开启调试模式，{false|true}
     suffix:     '.js',
-    alias: {
-        app: 'http://localhost:99/js/app/',
-        ext: 'http://localhost:99/js/ext/',
-        ven: 'http://localhost:99/vendor/',
-    },
     // 兼容 Require
     paths: {
-        //:'http://localhost:99/Tyui@0.1/js/units/md2html.js',
-        'marked':       'ven:marked.js',
-        'prism':        'ven:prism.js',
+        'marked':       'https://libs.cdnjs.net/marked/1.1.0/marked.min.js',
+        'prism':        'https://libs.cdnjs.net/prism/9000.0.1/prism.min.js',
         'katex':        'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js',
         'raphael':      'https://libs.cdnjs.net/raphael/2.3.0/raphael.min.js',
-        'flowchart':    'ven:flowchart-latest.js',
-        'md2x':         '../md2x.js?v=1.0',
+        'flowchart':    'https://libs.cdnjs.net/flowchart/1.13.0/flowchart.min.js',
+        'md2x':         'https://chun.ge/js/md2x.js',
         'mermaid':      'https://libs.cdnjs.net/mermaid/8.5.2/mermaid.min.js',
         'echarts':      'https://cdn.jsdelivr.net/npm/vditor@3.2.12/dist/js/echarts/echarts.min.js'
         // 多链接备用
